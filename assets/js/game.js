27ffdbe61c9636ae81c13234689e334a19eaafaa
Peter <warrenp11@gmail.com>
2022-06-14 14:49:15 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 // Player info
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -6,12 +12,19 @@ var playerMoney = 10;
 // console.log(playerName, playerAttack, playerHealth);
 
 // Enemy info
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+// console.log(enemyNames.length);
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//   console.log(enemyNames[i]);
+//   console.log(i);
+//   console.log(enemyNames[i] + " is at " + i + " index");
+// }
 
 // Fight function
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players they're starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -27,9 +40,9 @@ var fight = function () {
     console.log(
       playerName +
         " attacked " +
-        enemyName +
+        enemyNames[i] +
         ". " +
-        enemyName +
+        enemyNames[i] +
         " now has " +
         enemyHealth +
         " health remaining."
@@ -37,15 +50,17 @@ var fight = function () {
 
     // Check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(+" has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(
+        enemyNames[i] + " still has " + enemyHealth + " health left."
+      );
     }
 
     // Enemy attacks player
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName +
+      enemyNames[i] +
         " attacked " +
         playerName +
         ". " +
@@ -81,4 +96,8 @@ var fight = function () {
   }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
+// fight();
